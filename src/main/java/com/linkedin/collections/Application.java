994c95ddaf80de126c891e56296d0fa18fad1e12
kmb385 <kmb385@gmail.com>
2021-07-16 12:26:35 -0400

    Final

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,18 +1,26 @@
 package com.linkedin.collections;
 
+import java.util.Collection;
+import java.util.List;
+
 public class Application {
 
 	public static void main(String[] args) {
 	
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room manchester = new Room("Manchester", "Suite", 5, 250.0);
+		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
 
-		double total = getPotentialRevenue(cambridge, manchester);
+		Collection<Room> rooms = List.of(cambridge, manchester, piccadilly, oxford);
+		double total = getPotentialRevenue(rooms);
 		System.out.println(total);
 	}
 	
-	private static double getPotentialRevenue(Room room1, Room room2) {
-		return room1.getRate() + room2.getRate();
+	private static double getPotentialRevenue(Collection<Room> rooms) {
+		return rooms.stream()
+				.mapToDouble(r -> r.getRate())
+				.sum();
 	}
 
 }
