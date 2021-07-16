56ee1b6f7f5e43abdd67dce12883b96709275ea8
kmb385 <kmb385@gmail.com>
2021-07-16 12:12:35 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -7,11 +7,11 @@ public class Application {
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room manchester = new Room("Manchester", "Suite", 5, 250.0);
 
-		double total = getTotal(cambridge, manchester);
+		double total = getPotentialRevenue(cambridge, manchester);
 		System.out.println(total);
 	}
 	
-	private static double getTotal(Room room1, Room room2) {
+	private static double getPotentialRevenue(Room room1, Room room2) {
 		return room1.getRate() + room2.getRate();
 	}
 }
