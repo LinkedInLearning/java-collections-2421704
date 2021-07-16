d3c6136a016b6e9534339137d8462a9df1302a00
kmb385 <kmb385@gmail.com>
2021-07-16 14:15:48 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -4,13 +4,10 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		Contract contract1 = new Implementation();
-		printTerms(contract1);
 	}
 
 	private static void printTerms(Contract contract) {
-		contract.term1();
-		contract.term2();
+		
 	}
 
 }
--- src/main/java/com/linkedin/collections/Implementation.java
+++ src/main/java/com/linkedin/collections/Implementation.java
@@ -1,21 +1,6 @@
 package com.linkedin.collections;
 
-public class Implementation implements Contract{
+public class Implementation {
 
-	@Override
-	public void term1() {
-		System.out.println("Implementation:term1");
-	}
-
-	@Override
-	public void term2() {
-		System.out.println("Implementation:term2");
-	}
-
-	@Override
-	public void extendedTerm() {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
