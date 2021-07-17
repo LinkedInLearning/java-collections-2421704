31ab30f7d509cfddf8d97978ff43b08b72096a05
kmb385 <kmb385@gmail.com>
2021-07-17 09:31:50 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,9 +1,25 @@
 package com.linkedin.collections;
 
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.HashSet;
+
 public class Application {
 
 	public static void main(String[] args) {
-
+		
+		Collection<String> c = new ArrayDeque<>();
+		
+		c.add("snowboard");
+		c.add("running shoes");
+		c.add("linux");
+		c.add("books");
+		c.add("snowboard");
+		
+		Collection<String> favoriteThings = new HashSet<>(c);
+		
+		System.out.println(favoriteThings);
+		
 	}
 
 }
