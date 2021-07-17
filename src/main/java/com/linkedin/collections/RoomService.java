9cf2ac79a0a4f587c31255986de8ea242724311d
kmb385 <kmb385@gmail.com>
2021-07-17 13:50:58 -0400

    Updated

---
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -0,0 +1,40 @@
+package com.linkedin.collections;
+
+import java.util.Collection;
+
+public class RoomService {
+
+	// 1. Add Collection to store Room Inventory 
+
+	public RoomService() {
+		
+	// 2. Initialize Collection
+	
+	}
+
+	public Collection<Room> getInventory(){
+		
+	// 3. Return Room Inventory
+		
+		return null;
+	}
+	
+	public void createRoom(String name, String type, int capacity) {
+	
+	// 3. Add a new Room to Room Inventory
+	
+	}
+
+	public void createRooms(Room[] rooms) {
+	
+	// 4. Add Multiple Rooms to Room Inventory
+	
+	}
+	
+	public void removeRoom(Room room) {
+		
+	// 5. Remove Room From Room Inventory
+		
+	}
+
+}
---
+++ src/test/java/com/linkedin/collections/RoomServiceTest.java
@@ -0,0 +1,62 @@
+package com.linkedin.collections;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RoomServiceTest {
+
+	RoomService service;
+
+	Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
+	Room manchester = new Room("Manchester", "Suite", 5, 250.00);
+	Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+	Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+	Room victoria = new Room("Victoria", "Suite", 5, 225.00);
+	Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		this.service = new RoomService();
+
+		this.service.createRoom("Piccadilly", "Guest Room", 3);
+		this.service.createRoom("Cambridge", "Premiere Room", 3);
+		this.service.createRoom("Victoria", "Suite", 5);
+
+	}
+
+	@Test
+	void testCreateRoom() {
+
+		this.service.createRoom("Westminister", "Premiere Room", 4);
+
+		assertTrue(this.service.getInventory().size() == 4);
+	}
+
+	@Test
+	void testCreateRooms() {
+		Room[] newRooms = { this.westminister, this.oxford, this.manchester };
+
+		this.service.createRooms(newRooms);
+
+		assertTrue(this.service.getInventory().size() == 6);
+	}
+
+	@Test
+	void testRemoveRoom() {
+
+		this.service.removeRoom(new Room("Victoria", "Suite", 5, 225.00));
+
+		assertFalse(this.service.getInventory().contains(victoria));
+	}
+
+	@Test
+	void testGetInventory() {
+		assertNotNull(this.service.getInventory());
+	}
+
+}
