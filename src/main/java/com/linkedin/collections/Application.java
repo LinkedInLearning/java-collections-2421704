ac4f7e00f30173ec36a825c312c16f1c98938351
kmb385 <kmb385@gmail.com>
2021-07-17 15:58:14 -0400

    Updated

---
+++ src/main/java/com/linkedin/collections/Application.java
@@ -0,0 +1,16 @@
+package com.linkedin.collections;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
+		Room manchester = new Room("Manchester", "Suite", 5, 250.00);
+		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+		Room victoria = new Room("Victoria", "Suite", 5, 225.00);
+		Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
+		
+
+	}
+}
