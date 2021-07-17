bbeb4bd0a1ce2f9b8c72158604dcdbb23fc068cd
kmb385 <kmb385@gmail.com>
2021-07-17 14:10:38 -0400

    updated

--- src/main/java/com/linkedin/collections/Room.java
+++ src/main/java/com/linkedin/collections/Room.java
@@ -49,15 +49,41 @@ public class Room {
 		this.rate = rate;
 	}
 
-	public Room(String name, String type) {
-		super();
-		this.name = name;
-		this.type = type;
-	}
-
 	@Override
 	public String toString() {
 		return "Room [name=" + name + ", type=" + type + ", capacity=" + capacity + ", rate=" + rate + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(rate);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Room other = (Room) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(rate) != Double.doubleToLongBits(other.rate))
+			return false;
+		return true;
+	}
+	
+	
 
 }
