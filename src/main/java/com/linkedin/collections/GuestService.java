aa2c910fb81fb779f01539f4c9d429b2c758ae5d
kmb385 <kmb385@gmail.com>
2021-07-18 13:45:52 -0400

    updated

--- src/main/java/com/linkedin/collections/GuestService.java
+++ src/main/java/com/linkedin/collections/GuestService.java
@@ -2,7 +2,6 @@ package com.linkedin.collections;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GuestService {
 
@@ -13,9 +12,7 @@ public class GuestService {
 		// 1. Returns a new collection that contains guests from the provided collection 
 		// who have indicated the provided room is their first preference
 		
-		return guests.stream()
-				.filter(g -> g.getPreferredRooms().indexOf(room) == 0)
-				.collect(Collectors.toList());
+		return null; 
 
 	}
 
@@ -24,42 +21,13 @@ public class GuestService {
 		// 2. Adds a guest to the check in list, placing members of the loyalty program
 		// ahead of those guests not in the program, otherwise guests are ordered in the
 		// sequence they are added.
-		
-		if (guest.isLoyaltyProgramMember() && !this.checkInList.isEmpty()) {
-
-			int x = 0;
-			for (; x < this.checkInList.size(); x++) {
-
-				if (this.checkInList.get(x).isLoyaltyProgramMember()) {
-					continue;
-				}
-
-				break;
-
-			}
-
-			this.checkInList.add(x, guest);
-
-		} else {
-			this.checkInList.add(guest);
-		}
-
+	
 	}
 	
 	public void swapPosition(Guest guest1, Guest guest2) {
 		
 		// 3.  Swaps the position of the two provided guests within the check-in list.
 		// If guests are not currently in the list no action is required. 
-		
-		int position1 = this.checkInList.indexOf(guest1);
-		int position2 = this.checkInList.indexOf(guest2);
-		
-		if(position1 != -1 && position2 != -1) {
-			
-			this.checkInList.set(position1, guest2);
-			this.checkInList.set(position2, guest1);
-			
-		} 
 
 	}
 
