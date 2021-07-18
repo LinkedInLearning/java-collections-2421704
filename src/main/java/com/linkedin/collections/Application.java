c06333282ea6e1f4e70b8fda4e4acaf2243c2daa
kmb385 <kmb385@gmail.com>
2021-07-18 08:33:50 -0400

    updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,33 +1,13 @@
 package com.linkedin.collections;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
-		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
-		Room manchester = new Room("Manchester", "Suite", 5, 250.00);
-		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
-		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
-		Room victoria = new Room("Victoria", "Suite", 5, 225.00);
-		Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
+		List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
 		
-		Room oxfordDuplicate = new Room("Oxford", "Suite", 5, 225.0);
-		
-		Set<Room> otherRooms = Set.of(piccadilly, victoria);
-//		otherRooms.add(manchester);
-		
-		Set<Room> rooms = new HashSet<>();
-		rooms.add(piccadilly);
-		rooms.add(oxford);
-		rooms.add(oxford);
-		rooms.add(oxfordDuplicate);
-		
-		Set<Room> moreRooms = Set.copyOf(rooms);
-//		moreRooms.add(manchester);
-		
-		System.out.println(rooms);
 	}
 }
