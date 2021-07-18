77f7710ca00e5c1db851940e32030a5a7eac94cd
kmb385 <kmb385@gmail.com>
2021-07-18 08:38:55 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -2,12 +2,29 @@ package com.linkedin.collections;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
 		List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
+		
+		NavigableSet<Integer> numberTree = new TreeSet<>(numbers);
+		
+		numberTree.headSet(1750).stream()
+		.forEach(System.out::println);
+			
+		numberTree.tailSet(1750).stream()
+		.forEach(System.out::println);
+
+		numberTree.subSet(1750, 2750).stream()
+			.forEach(System.out::println);
+
+		System.out.println(numberTree.lower(1750));
+
+		System.out.println(numberTree.higher(1750));
 		
 	}
 }
