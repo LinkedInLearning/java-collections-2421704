3eaebde14b5b251dc0d46297386f425f59332ae4
kmb385 <kmb385@gmail.com>
2021-07-18 17:44:24 -0400

    updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,7 +1,5 @@
 package com.linkedin.collections;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Application {
@@ -13,21 +11,8 @@ public class Application {
 		Guest sonia = new Guest("Sonia", "Doe", true);
 		Guest siri = new Guest("Siri", "Doe", true);
 
-		Queue<Guest> checkinQueue = new PriorityQueue<>(Comparator.comparing(Guest::isLoyaltyProgramMember).reversed());
-		
-		checkinQueue.add(john);
-		checkinQueue.add(maria);
-		checkinQueue.add(sonia);
-		checkinQueue.add(siri);
 
-		Guest guest = checkinQueue.remove();
-		System.out.println(guest);
-		
-		Guest guest1 = checkinQueue.element();
-		System.out.println(guest1);
 
-		print(checkinQueue);
-		
 	}
 
 	public static void print(Queue<Guest> queue) {
