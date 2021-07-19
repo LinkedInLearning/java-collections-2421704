f9c50e11ee3688087b9766e4a3de471d39c413ed
kmb385 <kmb385@gmail.com>
2021-07-19 19:32:24 -0400

    updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,32 +1,39 @@
 package com.linkedin.collections;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Queue;
 
 public class Application {
 
 	public static void main(String[] args) {
 
+		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+		Room westminister = new Room("Westminister", "Premiere Room", 4, 175.00);
+		Room manchester = new Room("Manchester", "Suite", 5, 225.0);
+
 		Guest john = new Guest("John", "Doe", false);
-		Guest maria = new Guest("Maria", "Doe", false);
+		Guest maria = new Guest("Maria", "Doe", true);
 		Guest sonia = new Guest("Sonia", "Doe", true);
-		Guest siri = new Guest("Siri", "Doe", true);
-
-		Queue<Guest> checkinQueue = new PriorityQueue<>(Comparator.comparing(Guest::isLoyaltyProgramMember).reversed());
 		
-		checkinQueue.add(john);
-		checkinQueue.add(maria);
-		checkinQueue.add(sonia);
-		checkinQueue.add(siri);
-
-		Guest guest = checkinQueue.remove();
-		System.out.println(guest);
+		Map<Room, Guest> roomAssignmentMap = new HashMap<>();
 		
-		Guest guest1 = checkinQueue.element();
-		System.out.println(guest1);
-
-		print(checkinQueue);
+		roomAssignmentMap.put(piccadilly, john);
+		roomAssignmentMap.put(westminister, maria);
+		roomAssignmentMap.put(manchester, sonia);
+		
+		System.out.println(roomAssignmentMap.get(manchester));
+	
+		Guest guest = roomAssignmentMap.remove(piccadilly);
+		roomAssignmentMap.putIfAbsent(piccadilly, roomAssignmentMap.remove(westminister));
+		roomAssignmentMap.putIfAbsent(westminister, guest);
+		
+		for(Entry<Room, Guest> entry : roomAssignmentMap.entrySet()) {
+			Room r = entry.getKey();
+			Guest g = entry.getValue();
+			System.out.format("%s : %s %s%n", r.getName(), g.getFirstName(), g.getLastName());
+		}
 		
 	}
 	
