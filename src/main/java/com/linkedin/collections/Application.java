4f1f501847c2c7bd7490147005ec5f760d546b56
kmb385 <kmb385@gmail.com>
2021-07-19 04:11:15 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,43 +1,24 @@
 package com.linkedin.collections;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Deque;
 
 public class Application {
 
 	public static void main(String[] args) {
 
-		Guest john = new Guest("John", "Doe", false);
-		Guest maria = new Guest("Maria", "Doe", false);
-		Guest sonia = new Guest("Sonia", "Doe", true);
-		Guest siri = new Guest("Siri", "Doe", true);
 
-		Queue<Guest> checkinQueue = new PriorityQueue<>(Comparator.comparing(Guest::isLoyaltyProgramMember).reversed());
-		
-		checkinQueue.add(john);
-		checkinQueue.add(maria);
-		checkinQueue.add(sonia);
-		checkinQueue.add(siri);
-
-		Guest guest = checkinQueue.remove();
-		System.out.println(guest);
-		
-		Guest guest1 = checkinQueue.element();
-		System.out.println(guest1);
-
-		print(checkinQueue);
-		
 	}
 
-	public static void print(Queue<Guest> queue) {
+	public static void print(Deque<String> deque) {
 
-		System.out.println(System.lineSeparator() + "--Queue Contents--");
+		System.out.format("%n--Deque Contents--%n");
 
 		int x = 0;
-		for(Guest guest : queue) {
-			System.out.format("%x: %s %s %n", x++, guest.toString(), x == 1 ? "(Head)":"");
+		for(String msg : deque) {
+			System.out.format("%x: %s %s %n", x++, msg, x == 1 ? "(Top)":"");
 		}
+		
+		System.out.println("");
 		
 	}
 }
