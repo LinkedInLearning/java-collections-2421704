7daaa3c8c1cbdb1d4d0b3b7daee23b1321b75639
kmb385 <kmb385@gmail.com>
2021-07-20 04:16:55 -0400

    Updated

--- src/main/java/com/linkedin/collections/BookingService.java
+++ src/main/java/com/linkedin/collections/BookingService.java
@@ -1,10 +1,8 @@
 package com.linkedin.collections;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class BookingService {
 
@@ -18,41 +16,21 @@ public class BookingService {
 
 	public void book(List<Guest> guests) {
 
-		List<Room> availableRooms = this.roomService.getInventory().stream()
-				.filter(r -> !bookings.containsKey(r))
-				.collect(Collectors.toList());
-
-		List<Room> preferredRooms = guests.stream()
-				.flatMap(g -> g.getPreferredRooms().stream())
-				.collect(Collectors.toList());
-
-		availableRooms.sort(Comparator.comparingInt(r -> preferredRooms.indexOf(r)));
-
-		for (Guest guest : guests) {
-
-			boolean booked = false;
-
-			for (Room room : guest.getPreferredRooms()) {
-
-				if (this.roomService.hasRoom(room)) {
-
-					if (bookings.putIfAbsent(room, guest) == null) {
-						booked = true;
-						availableRooms.remove(room);
-						break;
-					}
-
-				}
-
-			}
-
-			if (!booked) {
-
-				this.bookings.putIfAbsent(availableRooms.remove(0), guest);
-
-			}
-
-		}
+		/*
+		 * Basic:
+		 * 
+		 * Places the provided lists of guests into the bookings map as new Entry<Guest,Book>.
+		 * Each guest should ONLY be assigned to one room found in the Room Manager Inventory.
+		 * You can assume that guests.size =< roomManager.getInventory().size.  
+		 * 
+		 * Bonus:
+		 * 
+		 * Each guest has a List of preferred rooms. The List<Guest> is provided in
+		 * the order that guests have checked in, with guests earlier in the list
+		 * receiving priority in their room choice.  Optimize the associations placed into
+		 * the map so that guests receive their highest available preferred room.
+		 *  
+		 */
 
 	}
 
