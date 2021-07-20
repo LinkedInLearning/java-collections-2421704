5450e4e870075019ceb73205d4e906434b2405c9
kmb385 <kmb385@gmail.com>
2021-07-20 19:28:20 -0400

    updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,6 +1,7 @@
 package com.linkedin.collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -17,13 +18,33 @@ public class Application {
 		Room manchester = new Room("Manchester", "Suite", 5, 225.0);
 		
 		List<Room> rooms = new ArrayList<>(List.of(piccadilly, oxford1, cambridge, westminister, victoria, oxford, manchester));
-		Comparator<Room> priceComparator = Comparator.comparing(Room::getRate).thenComparing(Room::getType).reversed();
+		rooms.sort(Comparator.naturalOrder());
 
-		rooms.sort(priceComparator);
+//		Collections.shuffle(rooms);
+//		System.out.format("Random: %s%n", rooms.get(0).getName());
+		print(rooms);
+
 		
-		rooms.stream()
-			.forEach(r -> System.out.format("%s %s %f %n", r.getName(), r.getType(), r.getRate()));
-		
+		System.out.format("%nSwapped%n");
+		Collections.swap(rooms, 0, rooms.size()-1);
+		print(rooms);
+
+		System.out.format("%nReversed%n");
+		Collections.reverse(rooms);
+		print(rooms);
+
+		System.out.format("%nRotated%n");
+		Collections.rotate(rooms, 2);
+		print(rooms);
+
 	}
+	
+	private static void print(List<Room> rooms) {
+		System.out.format("%nList Contents%n");
+
+		rooms.stream()
+				.forEach(r -> System.out.format("%-15s %-15s %f %n", r.getName(), r.getType(), r.getRate()));
+	}
+
 
 }
