fb91afbe8978461e50105d60c527b168cc52cdf2
Kevin Bowersox <kmb385@gmail.com>
2021-07-24 12:48:05 -0400

    Updated

--- src/main/java/com/linkedin/collections/RoomService.java
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -1,40 +1,43 @@
 package com.linkedin.collections;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class RoomService {
 
-	// 1. Add Collection to store Room Inventory 
+	// 1. Add Collection to store Room Inventory
+	private Collection<Room> inventory;
 
 	public RoomService() {
 		
 	// 2. Initialize Collection
-	
+		this.inventory = new HashSet<>();
 	}
 
 	public Collection<Room> getInventory(){
 		
 	// 3. Return Room Inventory
 		
-		return null;
+		return new HashSet<>(this.inventory);
 	}
 	
 	public void createRoom(String name, String type, int capacity, double rate) {
 	
-	// 3. Add a new Room to Room Inventory
-	
+	// 4. Add a new Room to Room Inventory
+		this.inventory.add(new Room(name, type, capacity, rate));
 	}
 
 	public void createRooms(Room[] rooms) {
 	
-	// 4. Add Multiple Rooms to Room Inventory
-	
+	// 5. Add Multiple Rooms to Room Inventory
+		this.inventory.addAll(Arrays.asList(rooms));
 	}
 	
 	public void removeRoom(Room room) {
 		
-	// 5. Remove Room From Room Inventory
-		
+	// 6. Remove Room From Room Inventory
+		this.inventory.remove(room);
 	}
 
 }
--- src/test/java/com/linkedin/collections/RoomServiceTest.java
+++ src/test/java/com/linkedin/collections/RoomServiceTest.java
@@ -3,7 +3,6 @@ package com.linkedin.collections;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
