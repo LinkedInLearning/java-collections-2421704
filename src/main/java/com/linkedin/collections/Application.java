18b67fc4511c97b8c1dd4a7db014236b5b0614c0
Kevin Bowersox <kmb385@gmail.com>
2021-07-24 09:30:31 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,21 +1,8 @@
 package com.linkedin.collections;
 
-import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.HashSet;
-
 public class Application {
 
 	public static void main(String[] args) {
-		
-		Collection<String> c = new ArrayDeque<>();
-		c.add("snowboard");
-		c.add("linux");
-		c.add("books");
-		c.add("snowboard");
-		
-		Collection<String> favoriteThings = new HashSet<>(c);
-		System.out.println(favoriteThings);
 		
 		
 	}
