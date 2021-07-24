5f01acf8266e29c89aeeea50e2313c062bf34d25
Kevin Bowersox <kmb385@gmail.com>
2021-07-24 12:34:32 -0400

    Updated

--- src/main/java/com/linkedin/collections/Car.java
+++ src/main/java/com/linkedin/collections/Car.java
@@ -7,44 +7,6 @@ public class Car {
 	private String model;
 
 	private int mileage;
-	
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((make == null) ? 0 : make.hashCode());
-		result = prime * result + ((model == null) ? 0 : model.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		
-		Car c = (Car) obj;
-		System.out.format("Comparing %s %s with %s %s %n",
-				this.make, this.model, c.make, c.model);
-		
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Car other = (Car) obj;
-		if (make == null) {
-			if (other.make != null)
-				return false;
-		} else if (!make.equals(other.make))
-			return false;
-		if (model == null) {
-			if (other.model != null)
-				return false;
-		} else if (!model.equals(other.model))
-			return false;
-		return true;
-	}
 
 	public Car(String make, String model, int mileage) {
 		super();
