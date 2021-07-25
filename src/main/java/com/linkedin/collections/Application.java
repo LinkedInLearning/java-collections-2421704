66483234eb5e7ed9d09f47d1f6f341a67fdf1455
Kevin Bowersox <kmb385@gmail.com>
2021-07-25 16:41:27 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,8 @@
 package com.linkedin.collections;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Application {
 
 	public static void main(String[] args) {
@@ -8,7 +11,23 @@ public class Application {
 		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
 		Room oxfordDuplicate = new Room("Oxford", "Suite", 5, 225.0);
 		Room victoria = new Room("Victoria", "Suite", 5, 225.00);
-
+		
+		Set<Room> otherRooms = Set.of(piccadilly, oxford);
+//		otherRooms.add(victoria);
+		
+		
+		Set<Room> rooms = new LinkedHashSet<>();
+		rooms.add(piccadilly);
+		rooms.add(oxford);
+		rooms.add(oxford);
+		rooms.add(oxfordDuplicate);
+		rooms.add(victoria);
+		
+		Set<Room> moreRooms = Set.copyOf(rooms);
+		
+		moreRooms.stream()
+			.map(r -> r.getName())
+			.forEach(System.out::println);
 		
 	}
 }
