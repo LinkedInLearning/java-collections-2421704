c8012147b698b84e449f70307c0a2d986885b9f5
Kevin Bowersox <kmb385@gmail.com>
2021-07-25 09:39:27 -0400

    Revert "UPdated"
    This reverts commit 76f089e70e0925cd196afef83cb5ac6d604e158f.

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -13,10 +13,12 @@ public class Application {
 
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room manchester = new Room("Manchester", "Suite", 5, 250.00);
+		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
 		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
 		Room victoria = new Room("Victoria", "Suite", 5, 225.00);
+		Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
 
-		Collection<Room> rooms = new ArrayList<>(Arrays.asList(cambridge, oxford, victoria, manchester));
+		Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly, cambridge, westminister, oxford, victoria, manchester));
 		oxford.setPetFriendly(true);
 		victoria.setPetFriendly(true);
 		
@@ -25,17 +27,3 @@ public class Application {
 			.forEach(room -> System.out.println(room.getName()));
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
