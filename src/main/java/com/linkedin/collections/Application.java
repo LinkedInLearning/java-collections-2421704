419389a04f88194e9978771753854ab43b3d7713
Kevin Bowersox <kmb385@gmail.com>
2021-07-25 10:54:10 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -3,6 +3,8 @@ package com.linkedin.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Application {
 
@@ -17,13 +19,23 @@ public class Application {
 		oxford.setPetFriendly(true);
 		victoria.setPetFriendly(true);
 		
-		for(Room room : rooms) {
-			
-			if(room.isPetFriendly()) {
-				System.out.println(room.getName());
-			}
-			
-		}
+		rooms.stream()
+			.filter(new Predicate<Room>() {
+
+				@Override
+				public boolean test(Room room) {
+					System.out.format("Testing %s with result %b%n", room.getName(), room.isPetFriendly());
+					return room.isPetFriendly();
+				}
+				
+			}).forEach(new Consumer<Room>() {
+
+				@Override
+				public void accept(Room room) {
+					System.out.println(room.getName());
+				}
+				
+			});
 		
 	}
 }
