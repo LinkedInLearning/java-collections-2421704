dacfa5c9864b6ae6dbdecdaffeb441851649db08
Kevin Bowersox <kmb385@gmail.com>
2021-07-25 09:40:28 -0400

    test

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -3,7 +3,6 @@ package com.linkedin.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 
 public class Application {
 
@@ -11,12 +10,10 @@ public class Application {
 
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room manchester = new Room("Manchester", "Suite", 5, 250.00);
-		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
 		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
 		Room victoria = new Room("Victoria", "Suite", 5, 225.00);
-		Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
 
-		Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly, cambridge, westminister, oxford, victoria, manchester));
+		Collection<Room> rooms = new ArrayList<>(Arrays.asList(cambridge, oxford, victoria, manchester));
 		oxford.setPetFriendly(true);
 		victoria.setPetFriendly(true);
 		
@@ -30,3 +27,12 @@ public class Application {
 		
 	}
 }
+
+
+
+
+
+
+
+
+
