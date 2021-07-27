0287ae003947285d91f73e06aed13f875b450323
Kevin Bowersox <kmb385@gmail.com>
2021-07-27 05:00:34 -0400

    updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -2,12 +2,19 @@ package com.linkedin.collections;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
 		List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
+	
+		NavigableSet<Integer> numberTree = new TreeSet<>(numbers);
+		
+		System.out.println(numberTree.higher(750));
+		
 		
 	}
 }
