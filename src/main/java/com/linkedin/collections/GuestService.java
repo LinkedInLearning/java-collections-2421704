d4ca1316e3bbd1063c9b8096599f4a1554b2a846
Kevin Bowersox <kmb385@gmail.com>
2021-07-28 04:25:15 -0400

    updated

--- src/main/java/com/linkedin/collections/GuestService.java
+++ src/main/java/com/linkedin/collections/GuestService.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 public class GuestService {
 
-	private List<Guest> checkInList = new ArrayList<>(100);
+	private List<Guest> checkinList = new ArrayList<>(100);
 
 	public static List<Guest> filterByFavoriteRoom(List<Guest> guests, Room room) {
 
-		// 1. Returns a new collection that contains guests from the provided collection 
-		// who have indicated the provided room is their first preference
+		/*
+		 *  1. Returns a new collection that contains guests from the provided collection
+		 *  who have indicated the provided room as their first preference. 
+		 */
 		
 		return null; 
 
@@ -18,20 +20,25 @@ public class GuestService {
 
 	public void checkIn(Guest guest) {
 		
-		// 2. Adds a guest to the check in list, placing members of the loyalty program
-		// ahead of those guests not in the program, otherwise guests are ordered in the
-		// sequence they are added.
+		/*
+		 *  2. Adds a guest to the checkinList, placing members of the loyalty program
+		 *  ahead of those guests not in the program. Otherwise, guests are arranged in the
+		 *  order they were inserted.
+		 */
+
 	
 	}
 	
 	public void swapPosition(Guest guest1, Guest guest2) {
 		
-		// 3.  Swaps the position of the two provided guests within the check-in list.
-		// If guests are not currently in the list no action is required. 
+		/*
+		 *  3.  Swaps the position of the two provided guests within the checkinList.
+		 *  If guests are not currently in the list no action is required.
+		 */ 
 
 	}
 
 	public List<Guest> getCheckInList() {
-		return List.copyOf(this.checkInList);
+		return List.copyOf(this.checkinList);
 	}
 }
