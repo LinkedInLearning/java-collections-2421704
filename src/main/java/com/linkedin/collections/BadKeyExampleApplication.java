0ed792dc144b245441edb990ddc69213b0491c30
Kevin Bowersox <kmb385@gmail.com>
2021-07-30 12:49:01 -0400

    Update

--- src/main/java/com/linkedin/collections/BadKeyExampleApplication.java
+++ src/main/java/com/linkedin/collections/BadKeyExampleApplication.java
@@ -18,7 +18,7 @@ public class BadKeyExampleApplication {
 		assignments.put(piccadilly, john);
 		assignments.put(oxford, maria);
 		
-		System.out.println("Piccadilly: " + assignments.get(piccadilly));
 		System.out.println("Oxford: " + assignments.get(new RoomWithBadKey("Oxford", "Suite", 5, 225.0)));
+		System.out.println("Piccadilly: " + assignments.get(piccadilly));
 	}
 }
