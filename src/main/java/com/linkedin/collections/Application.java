481429f0773a11d97691ea76f26c2cdbd2cf2ce2
Kevin Bowersox <kmb385@gmail.com>
2021-07-30 12:56:21 -0400

    Update

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,8 +1,5 @@
 package com.linkedin.collections;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Application {
 
 	public static void main(String[] args) {
@@ -13,13 +10,7 @@ public class Application {
 		Guest john = new Guest("John", "Doe", false);
 		Guest maria = new Guest("Maria", "Doe", true);
 
-		Map<Room,Guest> assignments = new HashMap<>();
 		
-		assignments.put(oxford, maria);
-		assignments.put(piccadilly, john);
-		
-		System.out.println(assignments.get(new Room("Oxford", "Suite", 5, 225.0)));
-		System.out.println(assignments.get(piccadilly));
 				
 	}
 
