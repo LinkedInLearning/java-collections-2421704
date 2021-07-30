f4b9ddf9bc01d589ecb5b6611a5bfbda369816f0
Kevin Bowersox <kmb385@gmail.com>
2021-07-30 11:51:17 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,12 +1,28 @@
 package com.linkedin.collections;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
+		Deque<String> messageStack = new ArrayDeque<>();
+		messageStack.push("Message 1");
+		messageStack.push("Message 2");
+		messageStack.push("Message 3");
+		messageStack.push("Message 4");
+
+		print(messageStack);
 		
+		System.out.println(messageStack.pop());
+		System.out.println(messageStack.pop());
+		print(messageStack);
+		messageStack.push("Message 5");
+		System.out.println(messageStack.peek());
+		
+		
+		print(messageStack);
 		
 	}
 
