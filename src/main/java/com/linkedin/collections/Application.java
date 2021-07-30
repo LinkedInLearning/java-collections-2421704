f5dd98cb88fc962b59dda244e4cb2101b187ed0a
Kevin Bowersox <kmb385@gmail.com>
2021-07-30 12:13:00 -0400

    updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -5,8 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
-		Room westminister = new Room("Westminister", "Premiere Room", 4, 175.00);
-		Room manchester = new Room("Manchester", "Suite", 5, 225.0);
+		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
 
 		Guest john = new Guest("John", "Doe", false);
 		Guest maria = new Guest("Maria", "Doe", true);
--- src/main/java/com/linkedin/collections/BadKeyExampleApplication.java
+++ src/main/java/com/linkedin/collections/BadKeyExampleApplication.java
@@ -7,21 +7,18 @@ public class BadKeyExampleApplication {
 
 	public static void main(String[] args) {
 		
-		RoomWithBadKey piccadilly = new RoomWithBadKey("Piccadilly", "Guest RoomWithBadKey", 3, 125.00);
-		RoomWithBadKey westminister = new RoomWithBadKey("Westminister", "Premiere RoomWithBadKey", 4, 175.00);
-		RoomWithBadKey manchester = new RoomWithBadKey("Manchester", "Suite", 5, 225.0);
-
+		RoomWithBadKey piccadilly = new RoomWithBadKey("Piccadilly", "Guest Room", 3, 125.00);
+		RoomWithBadKey oxford = new RoomWithBadKey("Oxford", "Suite", 5, 225.0);
+		
 		Guest john = new Guest("John", "Doe", false);
 		Guest maria = new Guest("Maria", "Doe", true);
-		Guest sonia = new Guest("Sonia", "Doe", true);
 
-		Map<RoomWithBadKey, Guest> RoomWithBadKeyAssignments = new HashMap<>();
+		Map<RoomWithBadKey, Guest> assignments = new HashMap<>();
 
-		RoomWithBadKeyAssignments.put(piccadilly, john);
-		RoomWithBadKeyAssignments.put(westminister, maria);
-		RoomWithBadKeyAssignments.put(manchester, sonia);
+		assignments.put(piccadilly, john);
+		assignments.put(oxford, maria);
 
-		System.out.println(RoomWithBadKeyAssignments.get(manchester));
-		System.out.println(RoomWithBadKeyAssignments.get(new RoomWithBadKey("Manchester", "Suite", 5, 225.0)));
+		System.out.println(assignments.get(piccadilly));
+		System.out.println(assignments.get(new RoomWithBadKey("Oxford", "Suite", 5, 225.0)));
 	}
 }
