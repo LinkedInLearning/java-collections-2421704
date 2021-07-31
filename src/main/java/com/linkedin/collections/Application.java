45f1b1997d1ae2bef3ef70c18dcbd761cfd8a238
Kevin Bowersox <kmb385@gmail.com>
2021-07-31 08:58:31 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -9,28 +9,20 @@ public class Application {
 
 		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
-		Room westminister = new Room("Westminister", "Premiere Room", 4, 175.00);
 		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
 		Room oxford1 = new Room("Oxford", "Guest Room", 5, 225.0);
 		Room victoria = new Room("Victoria", "Suite", 5, 225.0);
-		Room manchester = new Room("Manchester", "Suite", 5, 225.0);
 		
-		List<Room> rooms = new ArrayList<>(List.of(piccadilly, oxford1, cambridge, westminister, victoria, oxford, manchester));
+		List<Room> rooms = new ArrayList<>(List.of(piccadilly, oxford1, cambridge, victoria, oxford));
 
 		
 		
 		
 		
 		
-		
+		System.out.format("%n%n--List Contents--%n");
+		rooms.stream()
+			.forEach(r -> System.out.format("%-15s %-15s %-10f %n", r.getName(), r.getType(), r.getRate()));
 	}
 	
-	private static void print(List<Room> rooms) {
-		System.out.format("%nList Contents%n");
-
-		rooms.stream()
-				.forEach(r -> System.out.format("%-15s %-15s %f %n", r.getName(), r.getType(), r.getRate()));
-	}
-
-
 }
