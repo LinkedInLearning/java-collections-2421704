f5b58787e8b11b727a567d42638fc60a501fd657
Kevin Bowersox <kmb385@gmail.com>
2021-07-31 09:36:51 -0400

    updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,6 +1,7 @@
 package com.linkedin.collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
@@ -16,7 +17,13 @@ public class Application {
 		List<Room> rooms = new ArrayList<>(List.of(piccadilly, oxford1, cambridge, victoria, oxford));
 		rooms.sort(Room.RATE_COMPARATOR);
 		
+		Room newLondon = new Room("New London", "Suite", 5, 225.0);
+		int result = Collections.binarySearch(rooms, newLondon, Room.RATE_COMPARATOR);
+		rooms.add(Math.abs(++result), newLondon);
+		System.out.println("Result: " + result);
 		
+		System.out.println("Min: " + Collections.min(rooms, Room.RATE_COMPARATOR).getName());
+		System.out.println("Max: " + Collections.max(rooms, Room.RATE_COMPARATOR).getName());
 		
 		
 		
