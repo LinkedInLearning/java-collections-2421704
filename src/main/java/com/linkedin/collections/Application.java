a99990119252ce10e60d8ea43e4bd2b9e147cda4
Kevin Bowersox <kmb385@gmail.com>
2021-07-31 14:27:33 -0400

    Updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -7,8 +7,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		Collection c = new ArrayList(); //Same Collection Type, Different Element Type
-		c.add("Cambridge");
+		Collection<Room> c = new ArrayList<>(); //Same Collection Type, Different Element Type
+//		c.add("Cambridge");
+//		c.add(1L);
+		c.add(new Room("Cambridge", "Suite", 6, 250.0));
+		
+		c.stream()
+			.forEach(e -> System.out.println(e.getName()));
 		
 		Collection c2 = new ArrayList(); //Same Collection Type, Different Element Type
 		c2.add(new Room("Cambridge", "Suite", 6, 250.0));
