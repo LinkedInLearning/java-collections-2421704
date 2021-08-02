ae0a8d817b47cd369b21ff7a3c4f50494a6d7e3c
Kevin Bowersox <kmb385@gmail.com>
2021-08-02 05:11:47 -0400

    updated

--- src/main/java/com/linkedin/collections/BookingService.java
+++ src/main/java/com/linkedin/collections/BookingService.java
@@ -8,32 +8,29 @@ public class BookingService {
 
 	private Map<Room, Guest> bookings = new HashMap<>();
 
-	private RoomService roomService;
-
-	public BookingService(RoomService roomService) {
-		this.roomService = roomService;
-	}
-
-	public void book(List<Guest> guests) {
+	public boolean book(Room room, Guest guest) {
 
 		/*
-		 * Basic:
+		 * The provided Guest is placed in the bookings Map and
+		 * associated with the provided room, if no other guest
+		 * is associated with the room.
 		 * 
-		 * Places the provided lists of guests into the bookings map as new Entry<Guest,Book>.
-		 * Each guest should ONLY be assigned to one room found in the Room Manager Inventory.
-		 * You can assume that guests.size =< roomManager.getInventory().size.  
-		 * 
-		 * Bonus:
-		 * 
-		 * Each guest has a List of preferred rooms. The List<Guest> is provided in
-		 * the order that guests have checked in, with guests earlier in the list
-		 * receiving priority in their room choice.  Optimize the associations placed into
-		 * the map so that guests receive their highest available preferred room.
-		 *  
+		 * Returns a boolean that indicates if the Guest was
+		 * successfully placed in the room.
 		 */
-
+		
+		return false;
 	}
 
+	public double totalRevenue() {
+		
+		/*
+		 * Returns a double that totals the rate of each Room booked
+		 * in the bookings Map.
+		 */
+		return 0;
+	}
+	
 	public Map<Room, Guest> getBookings() {
 		return bookings;
 	}
--- src/test/java/com/linkedin/collections/BookingServiceTest.java
+++ src/test/java/com/linkedin/collections/BookingServiceTest.java
@@ -12,9 +12,6 @@ import org.junit.jupiter.api.Test;
 
 public class BookingServiceTest {
 
-
-	RoomService roomService;
-	GuestService guestService;
 	BookingService bookingService;
 
 	Room piccadilly, cambridge, westminister, oxford, victoria, manchester;
@@ -23,72 +20,48 @@ public class BookingServiceTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		
-		this.roomService = new RoomService();
-		this.guestService = new GuestService();
-		this.bookingService = new BookingService(this.roomService);
-
+		this.bookingService = new BookingService();
+		
 		piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
 		cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
 		oxford = new Room("Oxford", "Suite", 5, 225.0);
 		victoria = new Room("Victoria", "Suite", 5, 225.0);
 		manchester = new Room("Manchester", "Suite", 5, 250.0);
-		Room[] rooms = { piccadilly, cambridge, westminister, oxford, victoria, manchester };
-
-		this.roomService.createRooms(rooms);
 		
-
 		john = new Guest("John", "Doe", false);
 		maria = new Guest("Maria", "Doe", true);
 		sonia = new Guest("Sonia", "Doe", true);
 		siri = new Guest("Siri", "Doe", true);
 		bob = new Guest("Bob", "Doe", false);
 		sandra = new Guest("Sandra", "Doe", false);
-	
-		sonia.getPreferredRooms().addAll(List.of(cambridge, oxford));
-		maria.getPreferredRooms().addAll(List.of(cambridge, oxford));
-		john.getPreferredRooms().addAll(List.of(oxford, cambridge, manchester));
 	}
 
 	@Test
 	void testBook() {
-			
-		this.guestService.checkIn(bob);
-		this.guestService.checkIn(maria);
-		this.guestService.checkIn(sonia);
-		this.guestService.checkIn(john);
-		this.guestService.checkIn(siri);
 		
-		this.bookingService.book(this.guestService.getCheckInList());
-		this.bookingService.book(List.of(sandra));
-		
-		Map<Room, Guest> bookings = this.bookingService.getBookings();
-		assertEquals(6, bookings.size());
-		assertTrue(bookings.values().containsAll(List.of(bob,maria,sonia,john,siri,sandra)));
-		assertFalse(bookings.containsKey(null));
+		assertTrue(this.bookingService.book(cambridge, bob));
+		assertTrue(this.bookingService.book(oxford, maria));
+		assertTrue(this.bookingService.book(victoria, sonia));
+		assertFalse(this.bookingService.book(cambridge, siri));
+		assertFalse(this.bookingService.book(cambridge, sandra));
+		assertFalse(this.bookingService.book(oxford, john));
+		assertTrue(this.bookingService.book(victoria, john));
 			
 	}
+	
 	@Test
-	void testBookBonus() {
+	void testTotalRevenue() {
 		
-		this.guestService.checkIn(bob);
-		this.guestService.checkIn(maria);
-		this.guestService.checkIn(sonia);
-		this.guestService.checkIn(john);
-		this.guestService.checkIn(siri);
-		
-		this.bookingService.book(this.guestService.getCheckInList());
-		this.bookingService.book(List.of(sandra));
-		
-		Map<Room, Guest> bookings = this.bookingService.getBookings();
+		this.bookingService.book(piccadilly, john);
+		this.bookingService.book(oxford, maria);
+		this.bookingService.book(manchester, siri);
+		this.bookingService.book(victoria, sonia);
 
-		assertEquals(maria, bookings.get(cambridge));
-		assertEquals(sonia, bookings.get(oxford));
-		assertEquals(john, bookings.get(manchester));
+		assertEquals(825, this.bookingService.totalRevenue());
+		this.bookingService.book(cambridge, sandra);
 
-		assertTrue(List.of(siri, bob, sandra).contains(bookings.get(westminister)));
-		assertTrue(List.of(siri, bob, sandra).contains(bookings.get(victoria)));
-		assertTrue(List.of(siri, bob, sandra).contains(bookings.get(piccadilly)));
+		assertEquals(1000, this.bookingService.totalRevenue());
 		
 	}
 }
