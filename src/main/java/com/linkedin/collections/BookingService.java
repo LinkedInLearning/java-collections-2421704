e379304bf2743f7cf835b14750f1a2785ede76fa
Kevin Bowersox <kmb385@gmail.com>
2021-08-02 05:16:33 -0400

    updated

--- src/main/java/com/linkedin/collections/BookingService.java
+++ src/main/java/com/linkedin/collections/BookingService.java
@@ -1,7 +1,6 @@
 package com.linkedin.collections;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class BookingService {
--- src/test/java/com/linkedin/collections/BookingServiceTest.java
+++ src/test/java/com/linkedin/collections/BookingServiceTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
-import java.util.Map;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
